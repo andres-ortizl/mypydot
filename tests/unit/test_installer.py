130d73403009d6879a52d339c602751b7fa12093
Andrew <wilortiz@ucm.es>
2022-03-19 18:35:32 +0100

    remove temp installer test

--- tests/unit/test_installer.py
+++ tests/unit/test_installer.py
@@ -4,11 +4,12 @@ import pytest
 
 
 class TestOSPackageInstaller:
-    def test_mac_os_installer_fake_path(self):
-        with pytest.raises(SystemExit):
-            path = str(uuid.uuid4())
-            OSInstaller.install_homebrew_packages(path)
+    pass
+# def test_mac_os_installer_fake_path(self):
+#    with pytest.raises(SystemExit):
+#        path = str(uuid.uuid4())
+#        OSInstaller.install_homebrew_packages(path)
 
-    def test_fail_mac_os_in_linux(self):
-        path = './tests/unit/assets/brew_mock'
-        assert False is OSInstaller.install_homebrew_packages(path)
+# def test_fail_mac_os_in_linux(self):
+# path = './tests/unit/assets/brew_mock'
+# assert False is OSInstaller.install_homebrew_packages(path)
