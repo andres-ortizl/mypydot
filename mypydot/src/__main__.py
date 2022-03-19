5b7a43a82086b46b1684063e9c5443dbd4a1dbda
Andrés Ortiz <andres.ortiz@clarity.ai>
2022-03-19 11:54:25 +0100

    use click app to run main

--- mypydot/src/__main__.py
+++ mypydot/src/__main__.py
@@ -1,8 +1,29 @@
 from app import App
 from logging_manager import LoggingConf
+import click
+from cfg import Cfg
+
+
+@click.command()
+@click.argument('option')
+def main(option: str):
+    """Options can be :
+
+
+        - Create : It's going to initialize a new dotfiles folder and sync
+        the available symlinks
+
+        - Resync : Will try to sync new dotfiles
+
+        - Install : It's going to install an existing dotfiles folder
+
+        """
+    LoggingConf()
+    app = App()
+    f = app.parse_opt(option)
+    f()
+
 
 if __name__ == '__main__':
-    LoggingConf()
-    app = App(None)
-    f = app.parse_opt('init')
-    f()
+    main()
+
